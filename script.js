e5f1b9c5c8f5af02cfcf4622386f15dddca3b096
CLayNTC <opala888@outlook.com>
2026-06-28 23:28:12 -0300

    comentarios

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// Seleciona o formulário de contato
 const form = document.getElementById("contatoForm");
 
+// Validação do formulário
 form.addEventListener("submit", function(event) {
 
+    // Impede o envio automático
     event.preventDefault();
 
+    // Obtém os valores dos campos
     const nome = document.getElementById("nome").value.trim();
     const email = document.getElementById("email").value.trim();
     const mensagem = document.getElementById("mensagem").value.trim();
@@ -22,12 +26,15 @@ form.addEventListener("submit", function(event) {
 
     alert("Mensagem enviada com sucesso!");
 
+   
     form.reset();
 
-}); // fecha o formulário
+});
 
+// Seleciona o botão de troca de tema
 const botaoTema = document.getElementById("tema");
 
+// Alterna entre os temas claro e escuro
 botaoTema.addEventListener("click", () => {
     document.body.classList.toggle("dark");
 });
